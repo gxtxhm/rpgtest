e95e02e29522fac55212249e70fb951aec05f894
LJY0630 <deim123@naver.com>
2022-11-27 20:03:35 +0900

    UI
    UI

---
+++ Unity Project/Assets/Script/Inventory.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    
+    public GameObject inventoryPanel;
+    bool activeInventory = false;
+
+    public delegate void OnChangeItem();
+    public OnChangeItem onChangeItem;
+
+    List<Item> items = new List <Item>();
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I)) 
+        {
+            activeInventory = !activeInventory;
+            inventoryPanel.SetActive(activeInventory);
+        }
+    }
+
+    public bool AddItem(Item _item) 
+    {
+        if (items.Count < 9) 
+        {
+            items.Add(_item);
+            if (onChangeItem != null)
+              onChangeItem.Invoke();
+            return true;
+        }
+        return false;
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Get1");
+        if (collision.CompareTag("FieldItem")) 
+        {
+                Debug.Log("Get2");
+                FieldItem fieldItem = collision.GetComponent<FieldItem>();
+                if (AddItem(fieldItem.GetItem())) 
+                {
+                    fieldItem.DestroyItem();
+                }
+            }
+    }
+}
---
+++ Unity Project/Assets/Script/ItemDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public static ItemDatabase instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+    public List<Item> itemDB = new List<Item>();
+    [Space(20)]
+    public GameObject fieldItemPrefab;
+    public Vector3[] pos;
+
+    private void Start()
+    {
+        for (int i = 0; i < 5; i++) 
+        {
+            GameObject go = Instantiate(fieldItemPrefab, pos[i], Quaternion.identity);
+            go.GetComponent<FieldItem>().SetItem(itemDB[Random.Range(0,2)]);
+        }
+    }
+}
